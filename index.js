9c6dfc30f129dc4c96bed85d3ab45c0316ea4bc5
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-05 19:08:44 -0700

    changed structure

---
+++ index.js
@@ -0,0 +1,3 @@
+const static = require('static');         // Static HTML and CSS files
+const apps = require('apps');         // Helper JS files
+const BrowserWindow = require('BrowserWindow');         // BrowserWindow class
