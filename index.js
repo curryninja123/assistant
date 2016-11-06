257569bb7c78aeb9c1484e62bf2b96c454017c20
Vishnu Iyer <vishnu72498@gmail.com>
2016-11-05 21:54:32 -0700

    basic version running

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const static = require('static');         // Static HTML and CSS files
-const apps = require('apps');         // Helper JS files
-const BrowserWindow = require('BrowserWindow');         // BrowserWindow class
+const electron = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+const app = electron.app;
+const pages = "file://" + __dirname + "/static/pages/";
+const stylesheets = "file://" + __dirname + "/static/stylesheets/";
+var style = require('./static/stylesheets/style.json');
+var mainWindow = null;
+
+app.on('ready', function () {
+  console.log(style.colors.primary);
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    backgroundColor: style.colors.base1
+  });
+  mainWindow.loadURL(pages + "index.html");
+})
